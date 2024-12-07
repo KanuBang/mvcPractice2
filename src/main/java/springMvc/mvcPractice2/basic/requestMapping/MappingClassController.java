5e9b3fdd5ba20172403e746dfe4b8d99d8676e1e
gdrffg <gdrffg@gmail.com>
2024-12-07 16:32:11 +0900

    add: request mapping example

---
+++ src/main/java/springMvc/mvcPractice2/basic/requestMapping/MappingClassController.java
@@ -0,0 +1,33 @@
+package springMvc.mvcPractice2.basic.requestMapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    @GetMapping
+    public String users() {
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "find User= " + userId;
+    }
+
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return "update user= " + userId;
+    }
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId= " + userId;
+    }
+}
